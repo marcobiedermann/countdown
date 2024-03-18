a09c2efd746ae8f2cffaab772e5a1fd76d991e68
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com.>
2024-03-18 14:47:14 +0100

    feat: Settings Link
    Only link to the settings page if you are not on it, otherwise link back to the previous page

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,14 +1,27 @@
-import { FiSettings } from "react-icons/fi";
-import { Link, Outlet } from "react-router-dom";
+import { FiSettings, FiX } from "react-icons/fi";
+import { Link, Outlet, useLocation } from "react-router-dom";
 
 function Layout() {
+  const { pathname } = useLocation();
+
+  const isSettings = pathname === "/settings";
+
   return (
     <div className="app">
       <Outlet />
       <div className="fixed fixed--padded fixed--bottom fixed--right">
-        <Link to="/settings" className="button button--secondary button--round">
-          <FiSettings />
-        </Link>
+        {isSettings ? (
+          <Link to=".." className="button button--secondary button--round">
+            <FiX />
+          </Link>
+        ) : (
+          <Link
+            to="/settings"
+            className="button button--secondary button--round"
+          >
+            <FiSettings />
+          </Link>
+        )}
       </div>
     </div>
   );
