67cf5fbeb4b2db40621f3b93815d2f9939365f4f
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com.>
2024-04-08 17:02:08 +0200

    feat: Language Detector
    Add `i18n` language detector

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,4 +1,5 @@
 import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
 
@@ -10,7 +11,7 @@ const options = {
   lng: 'en',
 };
 
-i18n.use(initReactI18next).use(Backend).init(options);
+i18n.use(LanguageDetector).use(Backend).use(initReactI18next).init(options);
 
 export { options };
 export default i18n;
